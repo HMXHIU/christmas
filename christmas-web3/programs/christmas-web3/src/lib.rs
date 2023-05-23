860745e49405d96a340d5c6f618ca323ae413312
benjaminhon <benjaminhon86@gmail.com>
2023-05-23 20:45:02 +0800

    add to pool

--- christmas-web3/programs/christmas-web3/src/lib.rs
+++ christmas-web3/programs/christmas-web3/src/lib.rs
@@ -14,6 +14,16 @@ pub mod christmas_web3 {
         msg!("Merry Christmas!");
         Ok(())
     }
+
+    pub fn add_to_pool(ctx: Context<AddToPool>, amount: u64) -> Result<()> {
+        ctx.accounts.user_account.total_amount_contributed += amount;
+        msg!(
+            "Total amount contributed by {} = {}!",
+            ctx.accounts.user_account.key(),
+            ctx.accounts.user_account.total_amount_contributed
+        );
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -21,3 +31,22 @@ pub struct Initialize {}
 
 #[derive(Accounts)]
 pub struct SayHello {}
+
+#[derive(Accounts)]
+pub struct AddToPool<'info> {
+    #[account(
+        init_if_needed,  // create accounts if needed
+        payer = signer,  // signer = the person calling the API
+        seeds = [b"user_account", signer.key().as_ref()],
+        bump,
+        space = 8 + 8)] // first 8 bytes is reserved by anchor
+    pub user_account: Account<'info, UserAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct UserAccount {
+    total_amount_contributed: u64,
+}
