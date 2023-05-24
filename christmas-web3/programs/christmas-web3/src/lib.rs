3203c48623881dc6ec2c1fce95b4851adbfe946b
benjaminhon <benjaminhon86@gmail.com>
2023-05-24 22:08:09 +0800

    mint_token

--- christmas-web3/programs/christmas-web3/src/lib.rs
+++ christmas-web3/programs/christmas-web3/src/lib.rs
@@ -1,9 +1,16 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token;
+use anchor_spl::token::{Mint, MintTo, Token, TokenAccount, Transfer};
 
-declare_id!("F94qr5y8iCG8NNvcQpvJQVZvjcLhcHvQqwARytBjqe9T");
+declare_id!("5ZohsZtvVnjLy7TZDuujXneojE8dq27Y4mrsq3e8eKTZ");
 
 #[program]
 pub mod christmas_web3 {
+    use core::num;
+
+    use anchor_lang::system_program::transfer;
+    use anchor_spl::token::mint_to;
+
     use super::*;
 
     pub fn say_hello(ctx: Context<SayHello>) -> Result<()> {
@@ -18,6 +25,43 @@ pub mod christmas_web3 {
             ctx.accounts.user_account.key(),
             ctx.accounts.user_account.total_amount_contributed
         );
+        Ok(())
+    }
+
+    pub fn mint_token(ctx: Context<MintToken>, num_tokens: u64) -> Result<()> {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint_account.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::mint_to(cpi_ctx, num_tokens)?;
+
+        msg!(
+            "{} minted {} tokens using mint={}!",
+            ctx.accounts.signer.key(),
+            num_tokens,
+            ctx.accounts.mint_account.key()
+        );
+
+        Ok(())
+    }
+
+    pub fn transfer_token(ctx: Context<TransferToken>, num_tokens: u64) -> Result<()> {
+        let tx = Transfer {
+            from: ctx.accounts.from_account.to_account_info(),
+            to: ctx.accounts.to_account.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, tx);
+
+        token::transfer(cpi_ctx, num_tokens)?;
+
         Ok(())
     }
 }
@@ -37,6 +81,28 @@ pub struct AddToPool<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct MintToken<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub mint_account: Account<'info, Mint>,
+    #[account(mut)]
+    pub token_account: Account<'info, TokenAccount>, // User's Associated Token Account to hold the minted nft token
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct TransferToken<'info> {
+    #[account(mut)]
+    pub from_account: Account<'info, UserAccount>,
+    #[account(mut)]
+    pub to_account: Account<'info, UserAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub token_program: Program<'info, Token>,
 }
 
 #[account]
--- christmas-web3/tests/christmas-web3.ts
+++ christmas-web3/tests/christmas-web3.ts
@@ -3,6 +3,13 @@ import { Program } from "@coral-xyz/anchor";
 import { web3 } from "@coral-xyz/anchor";
 import { ChristmasWeb3 } from "../target/types/christmas_web3";
 import { assert } from "chai";
+import {
+  TOKEN_PROGRAM_ID,
+  MINT_SIZE,
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+  createInitializeMintInstruction,
+} from "@solana/spl-token";
 
 describe("christmas-web3", () => {
   // Configure the client to use the local cluster.
@@ -12,7 +19,7 @@ describe("christmas-web3", () => {
   const program = anchor.workspace.ChristmasWeb3 as Program<ChristmasWeb3>;
   const userAccount = anchor.web3.Keypair.generate();
 
-  it("Airdrops to user for payer", async () => {
+  it("Airdrop to user", async () => {
     const airdropSellerSig = await provider.connection.requestAirdrop(
       userAccount.publicKey,
       100e9
@@ -29,14 +36,14 @@ describe("christmas-web3", () => {
   /*
    *  sayHello
    */
-  it("Hello!", async () => {
+  it("Hello", async () => {
     const tx = await program.methods.sayHello().rpc();
   });
 
   /*
    *  addToPool
    */
-  it("Add to pool!", async () => {
+  it("Add to pool", async () => {
     // calculate the PDA of the user account
     const [pda, bump] = web3.PublicKey.findProgramAddressSync(
       [Buffer.from("user_account"), userAccount.publicKey.toBuffer()],
@@ -62,5 +69,75 @@ describe("christmas-web3", () => {
     // check totalAmountContributed
     const pdaInfo2 = await program.account.userAccount.fetch(pda);
     assert.ok(Number(pdaInfo2.totalAmountContributed) === 100);
+  });
+
+  /*
+    mintToken
+  */
+  it("Mint token", async () => {
+    // min rent for creating a mint
+    const mint_rent_lamports =
+      await program.provider.connection.getMinimumBalanceForRentExemption(
+        MINT_SIZE
+      );
+
+    // generate the mint keypair - because a mint is unique (it can only create 1 type of tokens)
+    const mintKey = anchor.web3.Keypair.generate();
+
+    // generate user's ATA key to hold the mint's tokens
+    const associated_token_account = await getAssociatedTokenAddress(
+      mintKey.publicKey,
+      userAccount.publicKey
+    );
+
+    const mint_tx = new anchor.web3.Transaction().add(
+      // create the mint account
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: userAccount.publicKey,
+        newAccountPubkey: mintKey.publicKey,
+        space: MINT_SIZE,
+        programId: TOKEN_PROGRAM_ID,
+        lamports: mint_rent_lamports,
+      }),
+      // initialize mint account properties
+      createInitializeMintInstruction(
+        mintKey.publicKey, // mint
+        0, // decimals
+        userAccount.publicKey, // mintAuthority
+        userAccount.publicKey // freezeAuthority
+      ),
+      createAssociatedTokenAccountInstruction(
+        userAccount.publicKey, // payer
+        associated_token_account, // ata
+        userAccount.publicKey, // owner
+        mintKey.publicKey // mint
+      )
+    );
+
+    const res = await provider.sendAndConfirm(
+      mint_tx,
+      [mintKey] // signers
+    );
+
+    console.log(
+      await provider.connection.getParsedAccountInfo(mintKey.publicKey)
+    );
+
+    const tx = await program.methods
+      .mintToken(new anchor.BN(100))
+      .accounts({
+        signer: userAccount.publicKey,
+        mintAccount: mintKey.publicKey,
+        tokenAccount: associated_token_account,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([userAccount])
+      .rpc();
+
+    const minted = (
+      await provider.connection.getParsedAccountInfo(associated_token_account)
+    ).value;
+
+    assert.ok(Number(minted) === 100);
   });
 });
