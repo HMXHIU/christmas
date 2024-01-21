e0c13285e8b2a27db9c93cace6b2a6ad5751fa73
benjaminhon <benjaminhon86@gmail.com>
2024-01-22 00:27:06 +0800

    CP-008-29: Debounce address search, use custom autocomplete

--- christmas/app-svelte/src/components/LocationSearch.svelte
+++ christmas/app-svelte/src/components/LocationSearch.svelte
@@ -9,6 +9,8 @@
         PopupSettings,
     } from "@skeletonlabs/skeleton";
 
+    import { throttle } from "lodash";
+
     export let label: string = "";
     export let address: string = "";
     export let region: string | null = null;
@@ -17,122 +19,119 @@
     export let longitude: number | null = null;
     export let geohash: string | null = null;
 
-    let locationInput: HTMLInputElement;
+    let map: HTMLDivElement;
 
-    onMount(() => {
-        setTimeout(async () => {
-            // Google maps
-            const loader = new Loader({
-                apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-                version: "weekly",
-            });
-            const placesLibrary = await loader.importLibrary("places");
+    let addressOptions: AutocompleteOption<string>[] = [];
 
-            // Initialize search box on input element (Note: `input` might not be rendered yet, thus the delay)
-            const searchBox = new placesLibrary.SearchBox(locationInput);
-            searchBox.addListener("places_changed", () => {
-                const places = searchBox.getPlaces();
-                if (places != null && places.length != 0) {
-                    const place = places[0];
-                    latitude = place.geometry?.location?.lat() || null;
-                    longitude = place.geometry?.location?.lng() || null;
-
-                    // set `address` for parent form
-                    address = place["formatted_address"] || "";
-                    const address_components =
-                        place["address_components"] || [];
-
-                    for (const component of address_components) {
-                        if (component.types.includes("country")) {
-                            [region, countryName] =
-                                COUNTRY_DETAILS[component.short_name];
-                            break;
-                        }
-                    }
-
-                    if (latitude != null && longitude != null) {
-                        geohash = ngeohash.encode(latitude, longitude, 6);
-                    }
-                }
-            });
-        }, 200);
-    });
-
-    let query: string = "";
     let popupSettings: PopupSettings = {
         event: "focus-click",
         target: "popupAutocomplete",
         placement: "bottom",
     };
 
-    function onPopupDemoSelect(event: Event) {
-        console.log(event);
+    let placesService: google.maps.places.PlacesService;
+
+    const findPlaceFromQuery = throttle((query) => {
+        placesService?.findPlaceFromQuery(
+            {
+                query: query || "",
+                fields: ["name", "formatted_address", "place_id"],
+            },
+            (results, status) => {
+                if (
+                    status === google.maps.places.PlacesServiceStatus.OK &&
+                    results
+                ) {
+                    // Update autocomplete options
+                    addressOptions = results.map(
+                        ({ formatted_address, name, place_id }) => {
+                            return {
+                                label: `${name}, ${formatted_address}`,
+                                value: place_id || "",
+                                meta: {
+                                    name,
+                                    formatted_address,
+                                    place_id,
+                                },
+                            };
+                        },
+                    );
+                } else {
+                    console.warn(status);
+                }
+            },
+        );
+    }, 1000);
+
+    function searchAddress(event: Event) {
+        findPlaceFromQuery((event.target as HTMLInputElement).value);
     }
 
-    const flavorOptions: AutocompleteOption<string>[] = [
-        {
-            label: "Vanilla",
-            value: "vanilla",
-            keywords: "plain, basic",
-            meta: { healthy: false },
-        },
-        {
-            label: "Chocolate",
-            value: "chocolate",
-            keywords: "dark, white",
-            meta: { healthy: false },
-        },
-        {
-            label: "Strawberry",
-            value: "strawberry",
-            keywords: "fruit",
-            meta: { healthy: true },
-        },
-        {
-            label: "Neapolitan",
-            value: "neapolitan",
-            keywords: "mix, strawberry, chocolate, vanilla",
-            meta: { healthy: false },
-        },
-        {
-            label: "Pineapple",
-            value: "pineapple",
-            keywords: "fruit",
-            meta: { healthy: true },
-        },
-        {
-            label: "Peach",
-            value: "peach",
-            keywords: "fruit",
-            meta: { healthy: true },
-        },
-    ];
+    function onSelectAddress(event: CustomEvent) {
+        const { place_id, name, formatted_address } = event.detail.meta;
+
+        address = `${name}, ${formatted_address}`;
+
+        placesService.getDetails(
+            {
+                placeId: place_id,
+                fields: ["address_components", "geometry.location"],
+            },
+            (place, status) => {
+                if (
+                    status == google.maps.places.PlacesServiceStatus.OK &&
+                    place
+                ) {
+                    const { geometry, address_components } = place;
+
+                    for (const component of address_components || []) {
+                        if (component.types.includes("country")) {
+                            [region, countryName] =
+                                COUNTRY_DETAILS[component.short_name];
+                            break;
+                        }
+                    }
+                    latitude = geometry?.location?.lat() || null;
+                    longitude = geometry?.location?.lng() || null;
+                    if (latitude != null && longitude != null) {
+                        geohash = ngeohash.encode(latitude, longitude, 6);
+                    }
+                }
+            },
+        );
+    }
+
+    onMount(async () => {
+        // Initialize google maps placesService
+        const loader = new Loader({
+            apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+            version: "weekly",
+        });
+        const placesLibrary = await loader.importLibrary("places");
+        placesService = new placesLibrary.PlacesService(map);
+    });
 </script>
 
 <label class="label" for="location">
     <span>{label}</span>
-    <input
-        id="location"
-        class="input"
-        type="text"
-        bind:this={locationInput}
-        bind:value={address}
-        placeholder="Your Store's Address"
-    />
+    <!-- Input -->
     <input
         class="input autocomplete"
         type="search"
         name="autocomplete-search"
         autocomplete="off"
-        bind:value={query}
-        placeholder="Search..."
+        value={address}
+        on:keyup={searchAddress}
+        placeholder="Search Address..."
         use:popup={popupSettings}
     />
+    <!-- Autocomplete options -->
     <div data-popup="popupAutocomplete" class="z-50 bg-surface-500">
         <Autocomplete
-            bind:input={query}
-            options={flavorOptions}
-            on:selection={onPopupDemoSelect}
+            bind:options={addressOptions}
+            on:selection={onSelectAddress}
         />
     </div>
 </label>
+
+<div bind:this={map} class="hidden w-0 h-0"></div>
