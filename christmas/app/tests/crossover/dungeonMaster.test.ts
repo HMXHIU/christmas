a5abd8445df051862d89fb493bf7c827e10d9844
benjaminhon <benjaminhon86@gmail.com>
2024-06-26 21:18:08 +0800

    CP-000: Refactor dungeonMaster tests

--- christmas/app/tests/crossover/dungeonMaster.test.ts
+++ christmas/app/tests/crossover/dungeonMaster.test.ts
@@ -2,148 +2,169 @@ import { INTERNAL_SERVICE_KEY } from "$env/static/private";
 import { borderingGeohashes } from "$lib/crossover/utils";
 import { monsterLimitAtGeohash } from "$lib/crossover/world/bestiary";
 import { spawnMonsters } from "$lib/server/crossover/dungeonMaster";
-import { monstersInGeohashQuerySet } from "$lib/server/crossover/redis";
+import {
+    initializeClients,
+    monstersInGeohashQuerySet,
+} from "$lib/server/crossover/redis";
 import type {
     Player,
     PlayerEntity,
 } from "$lib/server/crossover/redis/entities";
-import { expect, test } from "vitest";
+import { beforeAll, describe, expect, test } from "vitest";
 import { getRandomRegion } from "../utils";
 import { buffEntity, createRandomPlayer } from "./utils";
 
-test("Test DungeonMaster", async () => {
+const RESPAWN_ENDPOINT =
+    "http://localhost:5173/trpc/crossover.world.respawnMonsters";
+let playerOne: Player, playerTwo: Player, playerThree: Player;
+let uninhabitedGeohashes: string[];
+let maxMonstersInArea: number;
+
+beforeAll(async () => {
+    await initializeClients(); // create redis repositories
+
     const region = String.fromCharCode(...getRandomRegion());
 
-    // Player one
-    const playerOneName = "Gandalf";
-    const playerOneGeohash = "w21z3tss";
-    let [playerOneWallet, playerOneCookies, playerOne] =
-        await createRandomPlayer({
-            region,
-            geohash: playerOneGeohash,
-            name: playerOneName,
-        });
+    // Create players
+    [, , playerOne] = await createRandomPlayer({
+        region,
+        geohash: "w21z3tss",
+        name: "Gandalf",
+    });
 
-    // Player two
-    const playerTwoName = "Saruman";
-    const playerTwoGeohash = "w21z3ttk";
-    let [playerTwoWallet, playerTwoCookies, playerTwo] =
-        await createRandomPlayer({
-            region,
-            geohash: playerTwoGeohash,
-            name: playerTwoName,
-        });
+    [, , playerTwo] = await createRandomPlayer({
+        region,
+        geohash: "w21z3ttk",
+        name: "Saruman",
+    });
 
-    // Player three
-    const playerThreeName = "Sauron";
-    const playerThreeGeohash = "w21z3tk8";
-    let [playerThreeWallet, playerThreeCookies, playerThree] =
-        await createRandomPlayer({
-            region,
-            geohash: playerThreeGeohash,
-            name: playerThreeName,
-        });
+    [, , playerThree] = await createRandomPlayer({
+        region,
+        geohash: "w21z3tk8",
+        name: "Sauron",
+    });
 
-    // Test get all uninhabited neighbouring geohashes
-    const uninhabitedGeohashes = await borderingGeohashes([
-        playerOneGeohash.slice(0, -1),
-        playerTwoGeohash.slice(0, -1),
-        playerThreeGeohash.slice(0, -1),
+    // Get uninhabited neighboring geohashes
+    uninhabitedGeohashes = await borderingGeohashes([
+        "w21z3ts",
+        "w21z3tt",
+        "w21z3tk",
     ]);
-    expect(uninhabitedGeohashes.sort()).to.deep.equal(
-        [
-            "w21z3tg",
-            "w21z3tu",
-            "w21z3tv",
-            "w21z3ty",
-            "w21z3te",
-            "w21z3tw",
-            "w21z3t7",
-            "w21z3tm",
-            "w21z3tq",
-            "w21z3t5",
-            "w21z3th",
-            "w21z3tj",
-        ].sort(),
-    );
 
-    // Test monster limit in area
-    const maxMonstersInArea = uninhabitedGeohashes.reduce(
-        (acc, currentGeohash) => {
-            return monsterLimitAtGeohash(currentGeohash) + acc;
-        },
+    // Calculate max monsters in area
+    maxMonstersInArea = uninhabitedGeohashes.reduce(
+        (acc, currentGeohash) => monsterLimitAtGeohash(currentGeohash) + acc,
         0,
     );
-    expect(maxMonstersInArea).to.equal(41);
+});
 
-    // Test spawn monsters cannot exceed monster limit
-    await spawnMonsters([playerOne, playerTwo, playerThree] as PlayerEntity[]);
-    await spawnMonsters([playerOne, playerTwo, playerThree] as PlayerEntity[]);
-    await spawnMonsters([playerOne, playerTwo, playerThree] as PlayerEntity[]);
-
-    const numMonstersInArea = await Promise.all(
-        uninhabitedGeohashes.map((geohash) =>
-            monstersInGeohashQuerySet([geohash]).count(),
-        ),
-    ).then((monsterCounts) => {
-        return monsterCounts.reduce((acc, current) => acc + current, 0);
-    });
-    expect(numMonstersInArea).to.lessThan(maxMonstersInArea);
-
-    // Test world.respawnMonsters
-    await expect(
-        fetch("http://localhost:5173/trpc/crossover.world.respawnMonsters", {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${INTERNAL_SERVICE_KEY}`,
-            },
-        }).then((res) => res.json()),
-    ).resolves.toMatchObject({
-        result: {
-            data: {
-                status: "success",
-                time: expect.any(Number),
-            },
-        },
+describe("DungeonMaster Tests", () => {
+    test("Uninhabited neighbouring geohashes", () => {
+        expect(uninhabitedGeohashes.sort()).to.deep.equal(
+            [
+                "w21z3tg",
+                "w21z3tu",
+                "w21z3tv",
+                "w21z3ty",
+                "w21z3te",
+                "w21z3tw",
+                "w21z3t7",
+                "w21z3tm",
+                "w21z3tq",
+                "w21z3t5",
+                "w21z3th",
+                "w21z3tj",
+            ].sort(),
+        );
     });
 
-    // Test world.respawnMonsters unauthorized
-    let res = await fetch(
-        "http://localhost:5173/trpc/crossover.world.respawnMonsters",
-        {
+    test("Monster limit in area", () => {
+        expect(maxMonstersInArea).to.equal(41);
+    });
+
+    test("Spawn monsters cannot exceed monster limit", async () => {
+        await spawnMonsters([
+            playerOne as PlayerEntity,
+            playerTwo as PlayerEntity,
+            playerThree as PlayerEntity,
+        ]);
+        await spawnMonsters([
+            playerOne as PlayerEntity,
+            playerTwo as PlayerEntity,
+            playerThree as PlayerEntity,
+        ]);
+        await spawnMonsters([
+            playerOne as PlayerEntity,
+            playerTwo as PlayerEntity,
+            playerThree as PlayerEntity,
+        ]);
+
+        const numMonstersInArea = await Promise.all(
+            uninhabitedGeohashes.map((geohash) =>
+                monstersInGeohashQuerySet([geohash]).count(),
+            ),
+        ).then((monsterCounts) =>
+            monsterCounts.reduce((acc, current) => acc + current, 0),
+        );
+
+        expect(numMonstersInArea).to.lessThan(maxMonstersInArea);
+    });
+
+    test("world.respawnMonsters - authorized", async () => {
+        await expect(
+            fetch(RESPAWN_ENDPOINT, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${INTERNAL_SERVICE_KEY}`,
+                },
+            }).then((res) => res.json()),
+        ).resolves.toMatchObject({
+            result: {
+                data: {
+                    status: "success",
+                    time: expect.any(Number),
+                },
+            },
+        });
+    });
+
+    test("world.respawnMonsters - unauthorized", async () => {
+        const res = await fetch(RESPAWN_ENDPOINT, {
             method: "POST",
             headers: {
                 Authorization: `Bearer invalidtoken`,
             },
-        },
-    );
-    expect(res.status).to.equal(401);
-    await expect(res.json()).resolves.toMatchObject({
-        error: {
-            message: "UNAUTHORIZED",
-        },
+        });
+        expect(res.status).to.equal(401);
+        await expect(res.json()).resolves.toMatchObject({
+            error: {
+                message: "UNAUTHORIZED",
+            },
+        });
     });
 
-    // Test world.buffEntity
-    playerOne = (await buffEntity(playerOne.player, {
-        hp: 100,
-        mp: 100,
-        st: 100,
-        ap: 40,
-        buffs: ["haste"],
-        debuffs: ["poisoned"],
-    })) as Player;
-    await expect(playerOne).resolves.toMatchObject({
-        player: playerOne.player,
-        name: "Gandalf",
-        lgn: true,
-        loc: ["w21z3tss"],
-        lvl: 1,
-        hp: 100,
-        mp: 100,
-        st: 100,
-        ap: 40,
-        dbuf: ["poisoned"],
-        buf: ["haste"],
+    test("world.buffEntity", async () => {
+        playerOne = (await buffEntity(playerOne.player, {
+            hp: 100,
+            mp: 100,
+            st: 100,
+            ap: 40,
+            buffs: ["haste"],
+            debuffs: ["poisoned"],
+        })) as Player;
+
+        expect(playerOne).toMatchObject({
+            player: playerOne.player,
+            name: "Gandalf",
+            lgn: true,
+            loc: ["w21z3tss"],
+            lvl: 1,
+            hp: 100,
+            mp: 100,
+            st: 100,
+            ap: 40,
+            dbuf: ["poisoned"],
+            buf: ["haste"],
+        });
     });
 });
