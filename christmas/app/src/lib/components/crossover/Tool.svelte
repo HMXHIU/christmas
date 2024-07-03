469169e23df79ba7cbe9e1ef4010a09e8c1f9f92
benjaminhon <benjaminhon86@gmail.com>
2024-07-03 14:36:52 +0800

    CP-210: Nicer tool icons

--- christmas/app/src/lib/components/crossover/Tool.svelte
+++ christmas/app/src/lib/components/crossover/Tool.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import * as Tabs from "$lib/components/ui/tabs";
-    import { Box } from "lucide-svelte";
+    import { Box, Hammer, Sword } from "lucide-svelte";
     import ScrollArea from "../ui/scroll-area/scroll-area.svelte";
     import Abilities from "./Abilities.svelte";
     import Actions from "./Actions.svelte";
@@ -10,15 +10,15 @@
 </script>
 
 <Tabs.Root value={tool} orientation="vertical" class="w-full flex flex-row">
-    <Tabs.List class="flex flex-col h-full w-8 p-0">
-        <Tabs.Trigger value="inventory" class="h-8"
+    <Tabs.List class="flex flex-col h-full w-9 p-0">
+        <Tabs.Trigger value="inventory" class="h-9"
             ><Box class="h-4 w-4" /></Tabs.Trigger
         >
-        <Tabs.Trigger value="abilities" class="h-8"
-            ><Box class="h-4 w-4" /></Tabs.Trigger
+        <Tabs.Trigger value="abilities" class="h-9"
+            ><Sword class="h-4 w-4" /></Tabs.Trigger
         >
-        <Tabs.Trigger value="actions" class="h-8"
-            ><Box class="h-4 w-4" /></Tabs.Trigger
+        <Tabs.Trigger value="actions" class="h-9"
+            ><Hammer class="h-4 w-4" /></Tabs.Trigger
         >
     </Tabs.List>
     <div class="flex-grow">
